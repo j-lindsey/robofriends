213425515572962882a1b58bf5d30b6259718b7a
j-lindsey <j-lindsey@users.noreply.github.com>
2020-09-13 20:53:11 -0600

    Updates

---
+++ precache-manifest.1e778c0161e9152817050674d42ec241.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f054f7fe0b97794deed5541026c60f14",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "91a5c81d6c11f74b23ac",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "36514f5e9e54f07284fd",
+    "url": "/robofriends/static/css/main.1d09ecf9.chunk.css"
+  },
+  {
+    "revision": "91a5c81d6c11f74b23ac",
+    "url": "/robofriends/static/js/2.364b9e27.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/robofriends/static/js/2.364b9e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36514f5e9e54f07284fd",
+    "url": "/robofriends/static/js/main.74b25bc3.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
